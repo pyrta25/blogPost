fc88bb0945ab94b0c99db390c27f87bea83813c6
PyrtaBlog <pyrtaal@gmail.com>
2025-09-03 13:11:55 +0400

    Настройка проекта, создание моделей приложения blog

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.utils import timezone
+
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
