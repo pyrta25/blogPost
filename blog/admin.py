146b8814d2aaf5ce24e13ab349ae33e2e357946f
PyrtaBlog <pyrtaal@gmail.com>
2025-09-03 23:40:00 +0400

    Прописана модель поста, кастомные менеджеры, и функции вьюшек для темплэйтов

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from blog.models import Post
+
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'author', 'publish']
+    search_fields = ['title', 'body']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
