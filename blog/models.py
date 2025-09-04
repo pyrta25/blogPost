cfe9ccf3f8084dda5c57d6c753f6cc204d7b35f5
PyrtaBlog <pyrtaal@gmail.com>
2025-09-04 13:12:41 +0400

    Немного доработал urls приложения блог

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.urls import reverse
 from django.utils import timezone
 
 from django.db import models
@@ -19,7 +20,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -39,3 +40,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[str(self.id)])
